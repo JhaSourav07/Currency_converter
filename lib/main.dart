a90754d4907d4ed471cf00b3e1dd78c8ea7a1e1b
Sourav <souravkjha2007@gmail.com>
2025-11-03 01:27:29 +0530

    Initital App | Building While Learning

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Text("Hello World", textDirection: TextDirection.ltr));
+}
+
+//Types of widgets
+
+class MyApp extends StatelessWidget {}
