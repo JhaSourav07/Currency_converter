4c525ccc4f8fceca839fc7f43882ecaf3570a45a
Sourav <souravkjha2007@gmail.com>
2025-11-05 01:26:14 +0530

    5th nov commit

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -3,22 +3,49 @@ import 'package:flutter/material.dart';
 class CurrencyConverterMaterialPage extends StatelessWidget {
   const CurrencyConverterMaterialPage({super.key});
 
-  @override
+  @override 
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final border = OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(40)),
+      borderSide: BorderSide(
+        color: Colors.red, 
+        width: 2,
+        style: BorderStyle.none,
+        strokeAlign: BorderSide.strokeAlignInside,
+      ),
+    );
+
+
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
       body: Center(
-        
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-            Text(
+            const Text(
               '0',
               style: TextStyle(
                 fontSize: 35,
-                color: Colors.black,
+                color: Colors.white,
                 fontWeight: FontWeight.bold,
               ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Enter amount in USD",
+                hintStyle: const TextStyle(
+                  color: Colors.black,
+                ),
+                prefixIcon: const Icon(
+                  Icons.attach_money,
+                  color: Colors.black,
+                ),
+                filled: true,
+                fillColor: Colors.white24,
+                focusedBorder: border,
+                enabledBorder: border,
+              ),
+              keyboardType: TextInputType.number,
             ),
           ],
         ),
